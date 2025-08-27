a002da0e0c173470ef338a1dbc76fd818732de91
TEKURU-ZENO <132201660+TEKURU-ZENO@users.noreply.github.com>
2025-08-27 18:10:15 +0530

    Update login.ts

--- api/login.ts
+++ api/login.ts
@@ -1,12 +1,17 @@
 // api/login.ts
+import { VercelRequest, VercelResponse } from "@vercel/node";
 import jwt from "jsonwebtoken";
 
-export default function handler(req, res) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method === "POST") {
     const { password } = req.body;
 
     if (password === process.env.ADMIN_PASSWORD) {
-      const token = jwt.sign({ role: "admin" }, process.env.HWT_SECRET!, { expiresIn: "1h" });
+      const token = jwt.sign(
+        { role: "admin" },
+        process.env.HWT_SECRET as string,
+        { expiresIn: "1h" }
+      );
       res.status(200).json({ token });
     } else {
       res.status(401).json({ error: "Invalid password" });
